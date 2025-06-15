af98097a391f3b634a8929cd195ffbc4625e38ff
RaniPrasanna47 <raniprasanna997@gmail.com>
2025-06-15 14:58:43 +0530

    Fix

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -3,10 +3,8 @@ import axios from "axios";
 const API_KEY = "75440bac"; 
 const BASE_URL = "https://www.omdbapi.com/";
 
-// Search by title keyword with optional pagination
 export const searchMovies = async (query, page = 1) =>
   axios.get(`${BASE_URL}?apikey=${API_KEY}&s=${query}&page=${page}`);
 
-// Fetch full details for a single movie by IMDb ID
 export const getMovieDetails = async (id) =>
   axios.get(`${BASE_URL}?apikey=${API_KEY}&i=${id}&plot=full`);
